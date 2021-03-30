5f3b9a89359ecafee00eb445cf70be23736f9a34
Carlton Schuyler <carl@dragnon.com>
2021-03-30 17:48:51 -0600

    script to report on probably dead branches

---
+++ git-branches.sh
@@ -0,0 +1,31 @@
+## [WIP] list all branches which look like they can be removed from remote
+
+branches='list
+your
+brnach/names
+here'
+
+for branch in $branches
+do
+  set -e
+  branch_report="$(grep -e "origin/$branch\\$" branch-report.txt)"
+
+  if [[ -z "$(git ls-remote --heads origin "$branch")" ]]
+  then
+    echo "remote branch $branch doesn't exist: ## $branch_report"
+    continue
+  fi
+  { git checkout $branch --quiet 2>&1; } >/dev/null
+  { git reset --hard --quiet 2>&1; } >/dev/null
+
+  set +e
+  { git pull -r --quiet 2>&1; } >/dev/null
+  if [[ $? != 0 ]]
+  then
+    echo "remote branch $branch cannot be merged: ## $branch_report"
+  else
+    echo "git push origin \":$branch\" ## branch exists, run that <= to delete it ## $branch_report"
+  fi
+  set -e
+
+done
