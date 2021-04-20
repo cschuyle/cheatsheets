55275dd966d1ff0bb2971125d148bf929b021d61
Carlton Schuyler <carl@dragnon.com>
2021-04-20 07:37:18 -0600

    Fix git-branch-report to get actual remote branches only

--- git/git-branch-report.sh
+++ git/git-branch-report.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env bash
 set -e
 
+# NOTE: If you have not pruned your local repo (e.g. using git fetch --prune), then
+# a reference to any remote branch that existed whenever you did a fetch or pull is
+# stored in your local repo. So, use ls-remote to get actual remote branches.
+
 # TODO
 # Accept a list of branches to exclude from report. For example, master.
 
+exe="$(basename "$0")"
+output_file="$1"
+
 function usage() {
-  echo "Usage: git-branch-report.sh <output file>.csv
+  echo "Usage: $exe <output file>.csv
    $1
 "
 }
@@ -25,18 +32,21 @@ if [[ "$1" != *.csv ]] ; then
   exit 1
 fi
 
-OUTPUT_FILE="$1"
+origin=origin
 
 tmpdir="$(mktemp -d)"
 
-remote_branches="$tmpdir/remote-branches.txt"
+remote_branches="$tmpdir/actual-remote-branches.txt"
 merged_branches="$tmpdir/merged-branches.txt"
 
 git fetch
-git for-each-ref  refs/remotes/ --format='%(refname:short)'|sort > "$remote_branches"
-git branch -a --merged origin/master |sort|grep "^  remotes/origin"|sed 's/  remotes\///g' > "$merged_branches"
+
+# See NOTE
+git ls-remote --heads "$origin" | sed -E 's/\w+\s+refs\/heads\///' | xargs -L1 -I@ echo "$origin/"@ >> "$remote_branches"
+
+git branch -r --merged "$origin/master" |sort|grep "^  remotes/$origin"|sed 's/  remotes\///g' > "$merged_branches"
 
 # Output:
-echo '"Merged into master or not?","Date","Authors","Branch","Action"' >> "$OUTPUT_FILE"
-comm -12 "$remote_branches" "$merged_branches" | xargs -L1 -I@ sh -c "{ printf '\\\\\"@\\\\\",'; git show @ --format='\\\"%cs\\\",\\\"%an\\\"' -s ;}" | xargs -L1 -I@ echo '"MERGED",'@ >> "$OUTPUT_FILE"
-comm -23 "$remote_branches" "$merged_branches" | xargs -L1 -I@ sh -c "{ printf '\\\\\"@\\\\\",'; git show @ --format='\\\"%cs\\\",\\\"%an\\\"' -s ;}" | xargs -L1 -I@ echo '"UNMERGED",'@ >> "$OUTPUT_FILE"
+echo '"Merged into master or not?","Branch","Date","Authors","Action"' >> "$output_file"
+comm -12 "$remote_branches" "$merged_branches" | xargs -L1 -I@ sh -c "{ printf '\\\\\"@\\\\\",'; git show @ --format='\\\"%cs\\\",\\\"%an\\\"' -s ;}" | xargs -L1 -I@ echo '"MERGED",'@ >> "$output_file"
+comm -23 "$remote_branches" "$merged_branches" | xargs -L1 -I@ sh -c "{ printf '\\\\\"@\\\\\",'; git show @ --format='\\\"%cs\\\",\\\"%an\\\"' -s ;}" | xargs -L1 -I@ echo '"UNMERGED",'@ >> "$output_file"
